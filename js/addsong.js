ef2c1df9f9245a29c27bd3294de91d63dd7fb945
chaeyeoni0418 <chaeyeoni0418@gmail.com>
2026-05-29 23:43:58 +0900

    Add player and add song pages

---
+++ js/addsong.js
@@ -0,0 +1,38 @@
+﻿const songFileInput = document.querySelector("#songFileInput");
+const fileNameText = document.querySelector("#fileNameText");
+const uploadButton = document.querySelector("#uploadButton");
+const saveSongButton = document.querySelector("#saveSongButton");
+
+let selectedSongName = localStorage.getItem("seonyulSongName") || "";
+
+if (selectedSongName && fileNameText) {
+  fileNameText.textContent = selectedSongName;
+  fileNameText.classList.add("has-file");
+}
+
+uploadButton?.addEventListener("click", function () {
+  songFileInput?.click();
+});
+
+songFileInput?.addEventListener("change", function () {
+  const file = songFileInput.files && songFileInput.files[0];
+
+  if (!file) {
+    selectedSongName = "";
+    fileNameText.textContent = "파일을 올려주세요";
+    fileNameText.classList.remove("has-file");
+    return;
+  }
+
+  selectedSongName = file.name;
+  fileNameText.textContent = selectedSongName;
+  fileNameText.classList.add("has-file");
+});
+
+saveSongButton?.addEventListener("click", function () {
+  if (selectedSongName) {
+    localStorage.setItem("seonyulSongName", selectedSongName);
+  }
+
+  location.href = "player.html";
+});
